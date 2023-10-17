b9fe250afbb1bb52f257923e1030408dd26b5442
DarbinHdez <147088712+DarbinHdez@users.noreply.github.com>
2023-10-16 21:27:20 -0600

    Segunda version

--- Edad.java
+++ Edad.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 public class Edad {
 
 	public static void main(String[] args) {
+		//Definir mis variables
 		int año1;
 		int año2;
 		int edad;
-		
+		//Hacer el calculo de la edad
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Tu año de nacimiento: ");
 		año1 = sc.nextInt();
@@ -14,7 +15,7 @@ public class Edad {
 		año2 = sc.nextInt();
 		
 		edad= año2 - año1;
-		
+		//Mostrar mensaje de la edad actul del usuario
 		System.out.println("Usted tiene actualmente " + edad + " años");
 
 	}
